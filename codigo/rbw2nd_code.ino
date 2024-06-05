779df7b57314fe79a0ff4587a1c87fc048c5f06b
torquArt <157934500+torquArt@users.noreply.github.com>
2024-06-05 17:19:30 -0300

    Update rbw2nd_code.ino

--- codigo/rbw2nd_code.ino
+++ codigo/rbw2nd_code.ino
@@ -4,7 +4,17 @@
 #define sensorNoroeste x
 #define sensorNordeste x
 
+#include <servo.h>
+Servo servoEsquerda
+Servo servoDireita
+
+#define velHorario 0
+#define velAntiHorario 180
+
 void setup(){
+  servoEsquerda.attach(x);
+  servoDireita.attach(x);
+  
   pinMode(sensorFrente, INPUT);
   pinMode(sensorEsquerda, INPUT);
   pinMode(sensorDireita, INPUT);
@@ -21,15 +31,44 @@ void loop(){
   int leituraNoroeste = !digitalRead(sensorNordeste);
   int leituraNordeste = !digitalRead(sensorNoroeste);
 
-  Serial.print("leituraFrente: ");
-  Serial.print(leituraFrente);
-  Serial.print("leituraEsquerda: ");
-  Serial.print(leituraEsquerda);
-  Serial.print("leituraDireita: ");
-  Serial.print( leituraDireita);
   Serial.print("leituraNoroeste: ");
   Serial.print(leituraNoroeste);
+  Serial.print("leituraEsquerda: ");
+  Serial.print(leituraEsquerda);
+  Serial.print("leituraFrente: ");
+  Serial.print(leituraFrente);
+  Serial.print("leituraDireita: ");
+  Serial.print( leituraDireita);
   Serial.println("leituraNordeste: ");
+  Serial.println(leituraNordeste);
 
 
+  if (leituraEsquerda == 1 && leituraDireita == 1 && leituraFrente == 1 && leituraNordest e== 0 && leituraNoroeste == 0){
+   Serial.print("frente");
+   frente();
+  }
+  else {
+    parar();
+  }
+
+}
+
+
+/*---------------------------------------------------------------------------------------*/
+
+void frente(){
+  servoDireita.write(velHorario);
+  servoEsquerda.write(velHorario);
+}
+void parar(){
+  servoDireita.write(90);
+  servoEsquerda.write(90);
+}
+void esquerda(){
+  servoDireita.write(velHorario);
+  servoEsquerda.write(velAntiHorario);
+}
+void direita(){
+  servoDireita.write(velAntiHorario);
+  servoEsquerda.write(velHorario);
 }
