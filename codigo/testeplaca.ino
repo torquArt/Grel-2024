511d2758d30a2eee1568346d22bac9940efab052
torquArt <157934500+torquArt@users.noreply.github.com>
2024-07-04 14:29:10 -0300

    Update testeplaca.ino

--- codigo/testeplaca.ino
+++ codigo/testeplaca.ino
@@ -1,5 +1,4 @@
-void setup()
-
+void setup(){
 pinMode(4, OUTPUT);
 pinMode(5, OUTPUT);
 pinMode(6, OUTPUT);
@@ -7,8 +6,9 @@ pinMode(7, OUTPUT);
 pinMode(8, OUTPUT);
 
 Serial.begin(9600);
+}
 
-void loop()
+void loop(){
 Serial.print("4: ");
 Serial.print(analogRead(4));
 Serial.print(" /// 5: ");
@@ -19,3 +19,4 @@ Serial.print(" /// 7: ");
 Serial.print(analogRead(7));
 Serial.print(" /// 8: ");
 Serial.println(analogRead(8));
+}
