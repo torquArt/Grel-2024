fcb4115d7e7617836b41e3c943615284780786c0
torquArt <157934500+torquArt@users.noreply.github.com>
2024-07-10 13:00:08 -0300

    Update testeplaca.ino

--- codigo/testeplaca.ino
+++ codigo/testeplaca.ino
@@ -23,13 +23,13 @@ Serial.begin(9600);
 }
 
 void loop(){
-Serial.print(analogRead(A1));
+/*Serial.print(analogRead(A1));
+Serial.print(" /// ");*/
+Serial.print(digitalRead(A5));
 Serial.print(" /// ");
-Serial.print(analogRead(A2));
+Serial.print(digitalRead(A2));
 Serial.print(" /// ");
-Serial.print(analogRead(A3));
+Serial.print(digitalRead(A4));
 Serial.print(" /// ");
-Serial.print(analogRead(A4));
-Serial.print(" /// ");
-Serial.println(analogRead(A5));
+Serial.println(digitalRead(A3));
 }
