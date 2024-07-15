efebaaeb849bf214631ed7aacce7244e371a38aa
torquArt <157934500+torquArt@users.noreply.github.com>
2024-07-15 16:14:18 -0300

    Update testeplaca.ino

--- codigo/testeplaca.ino
+++ codigo/testeplaca.ino
@@ -13,7 +13,7 @@ Servo servoDireita;
 #define sensorEsq A3 // MAX: 632 MIN: 433
 #define sensorDir A4 // MAX: 486 MIN: 120
 #define sensorMesq A2 // MAX: 370 MIN: 102
-#define senosr Mdir A5 // MAX: 360 MIN: 120
+#define sensorMdir A5 // MAX: 360 MIN: 120
 
 int pinosSensores[5] = {A1, A2, A3, A4, A5};
 int valorF = 0;
@@ -34,35 +34,38 @@ Serial.begin(9600);
 
 
 void loop(){
-if (analogRead(sensorF) >= 300 ){
+if (analogRead(sensorF) > 300 ){
   valorF = 0;
-} else {valorF = 1;}
+} else{valorF = 1;}
 
 if (analogRead(sensorEsq) >= 400 ){
-  valorF = 0;
+  valorEsq = 0;
 } else {valorEsq = 1;}
   
 if (analogRead(sensorDir) >= 150 ){
-  valorF = 0;
+  valorDir = 0;
 } else {valorDir = 1;}
   
 if (analogRead(sensorMesq) >= 120 ){
-  valorF = 0;
+  valorMesq = 0;
 } else {valorMesq = 1;}
   
 if (analogRead(sensorMdir) >= 150 ){
-  valorF = 0;
+  valorMdir = 0;
 } else {valorMdir = 1;}
 
-Serial.print(valorF);
-/*Serial.print(analogRead(A3)); // esquerda
-Serial.print(" /// ");
-Serial.print(analogRead(A2)); // meio esquerda
-Serial.print(" /// ");
-Serial.print(analogRead(A1)); // frente 
-Serial.print(" /// ");
-Serial.print(analogRead(A5)); // meio direita
-Serial.print(" /// ");
-Serial.print(analogRead(A4)); // // direita*/
+Serial.print(analogRead(sensorF));
+Serial.print(" // ");
+Serial.print(valorEsq);
+//Serial.print(" // ");
+//Serial.print(valorMesq);
+//Serial.print(" // ");
+//Serial.print(valorF);
+//Serial.print(" // ");
+//Serial.print(valorMdir);
+//Serial.print(" // );
+//Serial.print(valorDir);
+//Serial.print(" // ");
+
 Serial.println();
 }
