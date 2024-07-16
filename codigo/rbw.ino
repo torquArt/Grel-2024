ae49128daf9e4ea59689292f03dd56323df5e0fe
torquArt <157934500+torquArt@users.noreply.github.com>
2024-07-15 23:30:58 -0300

    Update rbw.ino

--- codigo/rbw.ino
+++ codigo/rbw.ino
@@ -1,177 +1,134 @@
-/* código do Desafio de Robótica da SECT - 2023 */
+/* (invertido tem que ajeitar) cabo roxo longo, esquerdo
+cinza longo, meio esquerda
+roxo curto, meio direita
+cinza curto, direita
+amarelo, frente-*/
 
+#include <Servo.h>
 
-//1 = preto, 0 = branco
-#include <Ultrasonic.h>
+Servo servoEsquerda;
+Servo servoDireita;
 
-#define sensorEsquerda 12
-#define sensorDireita 4
-#define sensorEsquerda2 13
-#define sensorDireita2 5
-#define sensorMeio 8
+#define velEsq 180
+#define velDir 0
+#define velParar 90
 
-#define motorEsquerda_frente 11  //esquerda, frente
-#define motorEsquerda_antih 10  //esquerda, anti-horário
-#define motorDireita_frente 3   //direita, frente
-#define motorDireita_antih 9   //direira, anti-horário
+///////////////////////
 
-#define velocidade 60
-#define d 200
+#define sensorF A1 // MAX: 506 MIN: 202
+#define sensorEsq A3 // MAX: 632 MIN: 433
+#define sensorDir A4 // MAX: 486 MIN: 120
+#define sensorMesq A2 // MAX: 370 MIN: 102
+#define sensorMdir A5 // MAX: 360 MIN: 120
 
-#define dlvirar_d1 1800
-#define dlandar1 3000  //afastar
-#define dlvirar_e1 2000
-#define dlandar2 6000  //ultrapassar
-#define dlvirar_e2 2000
-#define dlandar3 500
-#define dlandar4 200  //voltar p/ linha
+//Ordem: Frente, esquerda, direita, meioesquerda e meiodireita.
+int sensoresMax[5] = {506, 632, 482, 370, 360}; // valores de cada sensor no branco
 
-int leituraEsquerda = !digitalRead(sensorEsquerda);
-int leituraDireita = !digitalRead(sensorDireita);
-int leituraEsquerda2 = !digitalRead(sensorEsquerda2);
-int leituraDireita2 = !digitalRead(sensorDireita2);
+int sensoresMin[5] = {202, 433, 120, 102, 120}; //valores de cada sensor no preto
 
-Ultrasonic ultra(A3, A2);
+int mediaSensores[5] = {0, 0, 0, 0, 0};
 
+int pinosSensores[5] = {A1, A3, A4, A2, A5};
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(sensorEsquerda, INPUT);
-  pinMode(sensorDireita, INPUT);
-  pinMode(sensorEsquerda2, INPUT);
-  pinMode(sensorDireita2, INPUT);
-  pinMode(sensorMeio, INPUT);
-  pinMode(motorEsquerda_frente, OUTPUT);
-  pinMode(motorEsquerda_antih, OUTPUT);
-  pinMode(motorDireita_frente, OUTPUT);
-  pinMode(motorDireita_antih, OUTPUT);
-}
+int valorF = 0;
+int valorEsq = 0;
+int valorDir = 0;
+int valorMesq = 0;
+int valorMdir = 0;
 
-void loop() {
-
-  int leituraEsquerda = !digitalRead(sensorEsquerda);
-  int leituraDireita = !digitalRead(sensorDireita);
-  int leituraEsquerda2 = !digitalRead(sensorEsquerda2);
-  int leituraDireita2 = !digitalRead(sensorDireita2);
-
-  //analogWrite(6, 100);
-  Serial.print(" leituraEsquerda2 = ");
-  Serial.print(leituraEsquerda2);
-  
-  Serial.print(" leituraEsquerda = ");
-  Serial.print(leituraEsquerda);
-  
-  Serial.print(" leituraDireita = ");
-  Serial.print(leituraDireita);
-  
-  Serial.print(" leituraDireita2 = ");
-  Serial.println(leituraDireita2);
-
-
-  if ((leituraEsquerda2 == 0 && leituraEsquerda == 0 && leituraDireita == 0 && leituraDireita2 == 0) ||
-      (leituraEsquerda2 == 0 && leituraEsquerda == 1 && leituraDireita == 1 && leituraDireita2 == 0) || 
-      (leituraEsquerda2 == 1 && leituraEsquerda == 1 && leituraDireita == 1 && leituraDireita2 == 1)) 
-  {
-        Serial.print("frente");
-        frente();
-  } 
-  
-  else if (leituraEsquerda2 == 0 && leituraEsquerda == 0 && leituraDireita == 1 && leituraDireita2 == 0) 
-  {
-    Serial.print("direita");
-    direita();
-  } 
-  
-  else if (leituraEsquerda2 == 0 && leituraEsquerda == 1 && leituraDireita == 0 && leituraDireita2 == 0)
-  {    
-    Serial.print("esquerda");
-    esquerda();
-  } 
-  
-  else if ((leituraEsquerda2 == 1 && leituraEsquerda == 0 && leituraDireita == 0 && leituraDireita2 == 0) ||
-           (leituraEsquerda2 == 1 && leituraEsquerda == 1 && leituraDireita == 0 && leituraDireita2 == 0) ||
-           (leituraEsquerda2 == 1 && leituraEsquerda == 0 && leituraDireita == 1 && leituraDireita2 == 0)) 
-  {            
-    Serial.print("noventa graus esquerda");
-    noventaGraus_esquerda();
-  } 
-  
-  else if ((leituraEsquerda2 == 0 && leituraEsquerda == 0 && leituraDireita == 0 && leituraDireita2 == 1) ||
-           (leituraEsquerda2 == 0 && leituraEsquerda == 0 && leituraDireita == 1 && leituraDireita2 == 1) || 
-           (leituraEsquerda2 == 0 && leituraEsquerda == 1 && leituraDireita == 0 && leituraDireita2 == 1)) 
-  {
-    Serial.print("noventa graus direita");
-    noventaGraus_direita();
+void setup(){
+//definindo os tcrt como saídas.
+  for(int i = 0; i<5; i++){
+    pinMode (pinosSensores[i], INPUT);
   }
+
+  servoEsquerda.attach(3);
+  servoDireita.attach(6);
+
+  Serial.begin(9600);
+}
+
+
+void loop(){
+
+  media(0); media(1); media(2); media(3); media(4);
+
+  if (analogRead(pinosSensores[0]) > mediaSensores[0] ){valorF = 0;} else{valorF = 1;}
+
+  if (analogRead(pinosSensores[1]) > mediaSensores[1] ){valorEsq = 0;} else {valorEsq = 1;}
+    
+  if (analogRead(pinosSensores[2]) > mediaSensores[2] ){valorDir = 0;} else {valorDir = 1;}
+    
+  if (analogRead(pinosSensores[3]) > mediaSensores[3] ){valorMesq = 0;} else {valorMesq = 1;}
+    
+  if (analogRead(pinosSensores[4]) > mediaSensores[4] ){valorMdir = 0;} else {valorMdir = 1;}
+
+  Serial.print(analogRead(sensorF)); Serial.print(" // ");
+  Serial.print(valorEsq); Serial.print(" // ");
+  Serial.print(valorMesq); Serial.print(" // ");
+  Serial.print(valorF); Serial.print(" // ");
+  Serial.print(valorMdir); Serial.print(" // ");
+  Serial.print(valorDir); Serial.print(" // ");
+
+  Serial.println();
+
+  if(valorEsq == 0 && valorMesq == 1 && valorF == 1 && valorMdir == 1 && valorDir == 0 ){
+    Serial.println("FRENTE");
+  }
+
+  else if((valorEsq == 1 && valorMesq == 1 && valorF == 0 && valorMdir == 0 && valorDir == 0) || 
+    (valorEsq == 1 && valorMesq == 0 && valorF == 0 && valorMdir == 0 && valorDir == 0)||
+    (valorEsq == 0 && valorMesq == 1 && valorF == 1 && valorMdir == 0 && valorDir == 0)){
+    Serial.println("ESQUERDA");
+  }
+
+  else if((valorEsq == 0 && valorMesq == 0 && valorF == 0 && valorMdir == 1 && valorDir == 1) || 
+    (valorEsq == 0 && valorMesq == 0 && valorF == 0 && valorMdir == 0 && valorDir == 1)||
+    (valorEsq == 0 && valorMesq == 0 && valorF == 1 && valorMdir == 1 && valorDir == 0)){
+      Serial.println("DIREITA");
+    }
+
+  else if(valorEsq == 0 && valorMesq == 1 && valorF == 0 && valorMdir == 1 && valorDir == 1){
+      Serial.println("NOV GRAUS DIREITA");
+    }
   
+  else if(valorEsq == 1 && valorMesq == 1 && valorF == 0 && valorMdir == 1 && valorDir == 0){
+      Serial.println("NOV GRAUS ESQUERDA");
+    }
+
+  else if(valorEsq == 1 && valorMesq == 1 && valorF == 0 && valorMdir == 1 && valorDir == 1){
+      Serial.println("T");
+    }
+
+  else if(valorEsq == 1 && valorMesq == 1 && valorF == 1 && valorMdir == 1 && valorDir == 1){
+      Serial.println("ENCRUZILHADA");
+    }
+
+  else{Serial.println("Sem condiçao");
+  }
 }
 
-
-void frente() {
-  analogWrite(motorEsquerda_frente, velocidade);
-  analogWrite(motorEsquerda_antih, 0);
-  analogWrite(motorDireita_frente, velocidade);
-  analogWrite(motorDireita_antih, 0);
-}
-void esquerda() {
-  analogWrite(motorEsquerda_frente, 0);
-  analogWrite(motorEsquerda_antih, velocidade);
-  analogWrite(motorDireita_frente, velocidade);
-  analogWrite(motorDireita_antih, 0);
-}
-void direita() {
-  analogWrite(motorEsquerda_frente, velocidade);
-  analogWrite(motorEsquerda_antih, 0);
-  analogWrite(motorDireita_frente, 0);
-  analogWrite(motorDireita_antih, velocidade);
+void media(int x){
+  mediaSensores[x] = (sensoresMax[x] + sensoresMin[x])/2;
 }
 
-void parar() {
-  analogWrite(motorEsquerda_frente, 0);
-  analogWrite(motorEsquerda_antih, 0);
-  analogWrite(motorDireita_frente, 0);
-  analogWrite(motorDireita_antih, 0);
+////////////////////
+
+void frente(){
+  servoEsquerda.write(velEsq);
+  servoDireita.write(velDir);
 }
 
-void noventaGraus_esquerda() {
-  frente();
-  delay(d);
-  while (!digitalRead(sensorMeio) == 0)
-    esquerda();
+void esquerda(){
+  servoEsquerda.write(velEsq);
+  servoDireita.write(velEsq);
 }
 
-void noventaGraus_direita() {
-  frente();
-  delay(d);
-  while (!digitalRead(sensorMeio) == 0)
-    direita();
+void direita(){
+  servoEsquerda.write(velDir);
+  servoDireita.write(velDir);
 }
-
-void desvio() {
-  direita();
-  delay(dlvirar_d1);
-
-  frente();
-  delay(dlandar1);
-
-  esquerda();
-  delay(dlvirar_e1);
-
-  frente();
-  delay(dlandar2);
-
-  esquerda();
-  delay(dlvirar_e2);
-
-  frente();
-  delay(dlandar3);
-
-  while (!digitalRead(sensorMeio) == 0)
-    frente();
-
-  frente();
-  delay(dlandar4);
-
-  while (!digitalRead(sensorMeio) == 0)
-    direita();
-}
+void novg_esquerda(){}
+void novg_direita(){}
+void t(){}
+void encruzilhada(){}
