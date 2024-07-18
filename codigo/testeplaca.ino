934291b68c0706a8d618068a9ba87ce81fb8606b
torquArt <157934500+torquArt@users.noreply.github.com>
2024-07-18 14:14:52 -0300

    Update testeplaca.ino

--- codigo/testeplaca.ino
+++ codigo/testeplaca.ino
@@ -1,13 +1,10 @@
-/* (invertido) cabo roxo longo, esquerdo
-cinza longo, meio esquerda
-roxo curto, meio direita
-cinza curto, direita
+// usar p/ calibrar os sensores
+
+/* cabo roxo longo, direita
+cinza longo, meio direita
+roxo curto, meio esquerda
+cinza curto, esquerda
 amarelo, frente-*/
-
-#include <Servo.h>
-
-Servo servoEsquerda;
-Servo servoDireita;
 
 #define sensorF A1 // MAX: 506 MIN: 202
 #define sensorEsq A3 // MAX: 632 MIN: 433
@@ -16,44 +13,16 @@ Servo servoDireita;
 #define sensorMdir A2 // MAX: 360 MIN: 120
 
 int pinosSensores[5] = {A1, A2, A3, A4, A5};
-int valorF = 0;
-int valorEsq = 0;
-int valorDir = 0;
-int valorMesq = 0;
-int valorMdir = 0;
 
 void setup(){
 for(int i = 0; i<5; i++){
 pinMode (pinosSensores[i], INPUT);
-}
-servoEsquerda.attach(3);
-servoDireita.attach(6);
 
 Serial.begin(9600);
 }
 
 
 void loop(){
-//if (analogRead(sensorF) > 300 ){
-//  valorF = 0;
-//} else if (analogRead(sensorF) < 300){valorF = 1;}
-//
-//if (analogRead(sensorEsq) >= 450){
-//  valorEsq = 0;
-//} else {valorEsq = 1;}
-//  
-//if (analogRead(sensorDir) >= 150 ){
-//  valorDir = 0;
-//} else {valorDir = 1;}
-//  
-//if (analogRead(sensorMesq) >= 120 ){
-//  valorMesq = 0;
-//} else {valorMesq = 1;}
-//  
-//if (analogRead(sensorMdir) >= 150 ){
-//  valorMdir = 0;
-//} else {valorMdir = 1;}
-//
 Serial.print(analogRead(sensorEsq));
 Serial.print(" // ");
 Serial.print(analogRead(sensorMesq));
